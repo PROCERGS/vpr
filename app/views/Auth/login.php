8564664cb9fbf603fe2de66af87962bfb86dfcaf
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2012-06-23 17:17:47 +0000

    Tentando fazer isso funcionar....
    git-svn-id: http://svn.procergs.reders/vpr/trunk@70 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- app/views/Auth/login.php
+++ app/views/Auth/login.php
@@ -1,18 +1,18 @@
 <?php require VIEWS_PATH . 'main.php'; ?>
 <?php startblock('title'); echo self::getTitle(); endblock(); ?>
-<?php startblock('main'); ?>
+<?php startblock('content'); ?>
 <form action="<?php echo $html->url(array('controller' => 'Auth', 'action' => 'login')) ?>" method="post">
 	<fieldset>
 		<div class="row">
 			<div class="twelvecol last">
 				<label for="titulo">Número do Título de Eleitor</label>
-				<input type="number" name="Cidadao[nro_titulo]" id="titulo" min="0" />
+				<input type="number" name="Cidadao[nro_titulo]" id="titulo" min="0" value="" />
 			</div>
 		</div>
 		<div class="row">
 			<div class="twelvecol last">
 				<label for="titulo">Número do RG</label>
-				<input type="number" name="Cidadao[rg]" id="titulo" min="0" />
+				<input type="number" name="Cidadao[rg]" id="titulo" min="0" value="" />
 			</div>
 		</div>
 	</fieldset>
@@ -22,5 +22,4 @@
 		</div>
 	</div>
 </form>
-<?php var_dump(Cidadao::validateRG_RS(4084575556)); ?>
 <?php endblock() ?>
