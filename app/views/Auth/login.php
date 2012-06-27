20cf3c77b04b8f737dad3d0706a9cd13bdaf5c49
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2012-06-27 11:54:34 +0000

    Oops... Isso não era pra ser commitado.
    git-svn-id: http://svn.procergs.reders/vpr/trunk@95 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- app/views/Auth/login.php
+++ app/views/Auth/login.php
@@ -6,13 +6,13 @@
 		<div class="row">
 			<div class="twelvecol last">
 				<label for="titulo">Número do Título de Eleitor</label>
-				<input type="number" name="Cidadao[nro_titulo]" id="titulo" min="0" value="101926780426" />
+				<input type="number" name="Cidadao[nro_titulo]" id="titulo" min="0" value="" />
 			</div>
 		</div>
 		<div class="row">
 			<div class="twelvecol last">
 				<label for="titulo">Número do RG</label>
-				<input type="number" name="Cidadao[rg]" id="titulo" min="0" value="4084575556" />
+				<input type="number" name="Cidadao[rg]" id="titulo" min="0" value="" />
 			</div>
 		</div>
 	</fieldset>
