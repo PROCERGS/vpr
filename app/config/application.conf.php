7ebf14c7ca9c6e04f3ea5b468712383d673eed47
procergs-diego-giacobo <procergs-diego-giacobo@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2012-06-28 15:31:41 +0000

    correção por Guilherme Donato
    git-svn-id: http://svn.procergs.reders/vpr/trunk@115 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- app/config/application.conf.php
+++ app/config/application.conf.php
@@ -3,5 +3,5 @@ PDOUtils::setDefaultDatabase('seplag_vpr');
 
 Config::set('votes.pageSize', 50);
 
-Config::set('sms.policy', SmsVote::ALLOW);
+Config::set('sms.policy', 'ALLOW');
 Config::set('sms.allowedRegionsId', array());
