c3f4ebab7d2b315abe8cb428eb499e6fe47cf5a5
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2012-06-29 17:30:30 +0000

    fixes #2257
    git-svn-id: http://svn.procergs.reders/vpr/trunk@123 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- app/config/application.conf.php
+++ app/config/application.conf.php
@@ -4,4 +4,4 @@ PDOUtils::setDefaultDatabase('seplag_vpr');
 Config::set('votes.pageSize', 50);
 
 Config::set('sms.policy', 'ALLOW');
-Config::set('sms.allowedRegionsId', array());
+Config::set('sms.allowedRegionsId', array(19, 22));
