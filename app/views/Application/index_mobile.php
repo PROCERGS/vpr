ad3eba37454172306a8a3aef60e869bca15f8c1a
procergs-rosane-leite <procergs-rosane-leite@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2012-07-01 21:27:37 +0000

    Inclusão da div titulo_pagina
    git-svn-id: http://svn.procergs.reders/vpr/trunk@149 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- app/views/Application/index_mobile.php
+++ app/views/Application/index_mobile.php
@@ -3,6 +3,7 @@
 <?php startblock('content'); ?>
 <div class="row">
 	<div class="twelvecol last home">
+		<div class="titulo_pagina">Votação de Prioridades<br />Orçamento 2013</div>
 		<div class="vote_now">
 <?php 	if ($show_vote_now)
 			echo $html->link("Para votar, clique aqui!", array('controller' => 'Election', 'action' => 'start'));
