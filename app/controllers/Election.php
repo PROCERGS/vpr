0f514a5fbd570de252000d847a5ad4b4413ff9ba
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2012-07-02 19:07:45 +0000

    Correção de problema do carregamento ultra lento das páginas em iPhones 3GS. refs #2258
    git-svn-id: http://svn.procergs.reders/vpr/trunk@177 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- app/controllers/Election.php
+++ app/controllers/Election.php
@@ -13,7 +13,6 @@ class Election extends AppController {
 	
 	protected static function setDefaultJavascripts() {
 		parent::setDefaultJavascripts();
-		self::addJavascript('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
 		self::addJavascript('/js/election.js');
 	}
 	
