0d8777459f1be10e5747ae7b2f1f2eae821c3325
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2012-07-02 22:35:46 +0000

    Inclusão das datas de votação na home mobile.
    git-svn-id: http://svn.procergs.reders/vpr/trunk@186 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- app/views/Application/index_mobile.php
+++ app/views/Application/index_mobile.php
@@ -2,6 +2,11 @@
 <?php startblock('title'); echo self::getTitle(); endblock(); ?>
 <?php startblock('page_class') ?>home_mobile<?php endblock() ?>
 <?php startblock('content'); ?>
+<div class="row online_voting_dates">
+	<div class="twelvecol last">
+		<p><?php echo $string_data; ?></p>
+	</div>
+</div>
 <div class="row">
 	<div class="twelvecol last home">
 		<div class="vote_now">
