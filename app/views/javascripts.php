7c214d3a78580ebf7a219d64f6650500ccd7bfa3
procergs-rosane-leite <procergs-rosane-leite@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2012-07-02 23:26:31 +0000

    Incluido codigo google analytics
    git-svn-id: http://svn.procergs.reders/vpr/trunk@188 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- app/views/javascripts.php
+++ app/views/javascripts.php
@@ -8,3 +8,18 @@
 	  foreach (Controller::getJavascripts() as $file) {?>
 		<script src="<?php echo $file; ?>"></script>
 <?php } ?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33078292-1']);
+  _gaq.push(['_setDomainName', 'rs.gov.br']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
