901412dc60ca356a1b2a034a6c0a027753348a66
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2012-07-03 17:56:20 +0000

    Fetch de votos via SMS na Comunika. refs #2300
    git-svn-id: http://svn.procergs.reders/vpr/trunk@199 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- app/controllers/Sms.php
+++ app/controllers/Sms.php
@@ -1,6 +1,8 @@
 <?php
 class Sms extends AppController {
 	
+	const SMS_NUMBER = 28506;
+	
 	public static function test() {
 		self::render();
 	}
@@ -19,6 +21,46 @@ class Sms extends AppController {
 		$msg    = self::getParam('msg');
 		$account= self::getParam('account');
 		
+		self::registerSMS($id_sms, $from, $to, $msg, $account);
+	}
+	
+	public static function fetch_messages() {
+		header("Content-Type:   text/html; charset=utf-8");
+		
+		$sms = new SMSAPI('seplag', 'ckxx55');
+		$messages = $sms->fetchMessages();
+		
+		$id_sms = 0;
+		$to = self::SMS_NUMBER;
+		$account = 0;
+		
+		foreach ($messages as $message) {
+			$message = explode("\t", $message);
+			
+			$from = $message[0];
+			$msg = $message[2];
+			
+			printr("Processando: [$from] $msg");
+			try {
+				self::registerSMS($id_sms, $from, $to, $msg, $account);
+			} catch (Exception $e) {
+				printr($e->getMessage());
+			}
+		}
+	}
+	
+	public static function send_messages() {
+		$sms = new SMSAPI('seplag', 'ckxx55');
+		$id = self::getParam('id');
+		if (is_numeric($id) && $id > 0)
+			$return = $sms->sendMessage($id, "555199674527", "Testando! :)");
+	
+		printr($return);
+	}
+	
+	private static function registerSMS($id_sms, $from, $to, $msg, $account) {
+		
+		
 		$msg = SmsVote::sanitizeMessage($msg);
 		$votacao = reset(Votacao::findByActiveVotacao());
 		
@@ -31,7 +73,7 @@ class Sms extends AppController {
 		try {
 			$sms_vote = new SmsVote($votacao, $id_sms, $from, $to, $msg, $account);
 		} catch (Exception $e) {
-			echo "RETORNO: ".$e->getMessage().". Informe apenas titulo, rg e códigos dos projetos separados por #.";
+			echo "RETORNO: ".$e->getMessage()." Informe apenas titulo, rg e códigos dos projetos separados por #.";
 			printr($e);
 			self::logError(new Cidadao(), new AppException($e->getMessage()), $e);
 			return;
@@ -47,13 +89,13 @@ class Sms extends AppController {
 				$exceeded_groups = join(', ', $exceeded_groups);
 				throw new ErrorException("Quantidade de votos excedida para os grupos: $exceeded_groups");
 			}
-			
+				
 			$invalid = $sms_vote->getInvalidOptions();
 			if (count($invalid) > 0) {
 				$invalid = join(', ', $invalid);
 				throw new ErrorException("As seguintes opção são inválidas: $invalid");
 			}
-			
+				
 			if ($sms_vote->registerVotes()) {
 				printr("Votos registrados! Obrigado por participar.");
 			}
---
+++ app/libs/SMSAPI.php
@@ -0,0 +1,96 @@
+<?php
+class SMSAPI {
+	
+	private $user, $password;
+	private $url = 'https://cgi2sms.com.br/3.0/';
+	
+	public function __construct($user, $password) {
+		$this->setUser($user);
+		$this->setPassword($password);
+	}
+	
+	public function apiCall($method, $params = array()) {
+		if (!empty($params))
+			$params = '?'.http_build_query($params);
+		else
+			$params = '';
+		
+		$url = $this->getURL();
+		$url .= "$method.php$params";
+		
+		if (Util::getEnvironmentName() == 'local')
+			$context = array('http' => array(
+				'proxy' => 'tcp://proxy.procergs.reders:3128',
+				'request_fulluri' => true,
+				'header' => "Proxy-Authorization: Basic ".Config::get('proxy.auth')
+			));
+		else
+			$context = array();
+		
+		$return = file_get_contents($url, NULL, stream_context_create($context));
+		$return = explode("\n", $return);
+		
+		$this->checkResult(reset($return));
+		
+		return $return;
+	}
+	
+	private function getDefaultParams() {
+		return array(
+			'user' => $this->getUser(),
+			'pass' => $this->getPassword(),
+			'linesep' => 0
+		);
+	}
+	
+	private function checkResult($code) {
+		switch ($code) {
+			case '00': 	return TRUE;
+						break;
+			case '08': throw new ErrorException("Erro de acesso a banco de dados.");
+			case '10': throw new ErrorException("Usuário/senha inválidos.");
+			case '11': throw new ErrorException("Parâmetros inválidos.");
+			case '12': throw new ErrorException("Número de telefone inválido ou não coberto pelo sistema");
+			case '13': throw new ErrorException("Operadora desativada para envio de mensagens");
+			case '14': throw new ErrorException("Usuário não pode enviar mensagens para esta operadora");
+			case '15': throw new ErrorException("Créditos insuficientes");
+			case '16': throw new ErrorException("Tempo mínimo entre duas requisições em andamento");
+			case '17': throw new ErrorException("Permissão negada para a utilização do CGI/Produtos");
+			case '18': throw new ErrorException("Operadora Offline");
+			case '19': throw new ErrorException("Envio negado a partir do IP de origem");
+			default: throw new ErrorException("Retorno inválido.");
+		}
+	}
+	
+	public function getUser() { return $this->user; }
+	private function setUser($user) { $this->user = $user; }
+	
+	private function getPassword() { return $this->password; }
+	private function setPassword($password) { $this->password = $password; }
+	
+	public function getURL() { return $this->url; }
+	
+	public function fetchMessages() {
+		$params = $this->getDefaultParams();
+		$messages = $this->apiCall('user_message_receive', $params);
+		array_shift($messages);
+		$count = array_shift($messages);
+		
+		array_pop($messages);
+		
+		if ($count != count($messages))
+			throw new ErrorException("Messages count missmatch. Expected $count messages but received ".count($messages));
+		
+		return $messages;
+	}
+	
+	public function sendMessage($id, $destination, $message) {
+		$params = $this->getDefaultParams();
+		$params['testmode'] = 0;
+		$params['messages'] = "\t$destination\t\t$message\t$id";
+		
+		$return = $this->apiCall('user_message_send', $params);
+		
+		return $return;
+	}
+}
