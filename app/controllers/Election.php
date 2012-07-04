8e60ed0b0fb011178bea81f33afa41f25bccad0f
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2012-07-04 00:07:13 +0000

    Revert. Estava com captcha!
    git-svn-id: http://svn.procergs.reders/vpr/trunk@216 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- app/controllers/Election.php
+++ app/controllers/Election.php
@@ -1,7 +1,6 @@
 <?php
-
 class Election extends AppController {
-
+	
 	public static function before() {
 		parent::before();
 		$action = self::getParam('action');
@@ -11,18 +10,18 @@ class Election extends AppController {
 		}
 		self::setPageName("Votação de Prioridades - Orçamento 2013");
 	}
-
+	
 	protected static function setDefaultJavascripts() {
 		parent::setDefaultJavascripts();
 		self::addJavascript('/js/election.js');
 	}
-
+	
 	public static function start() {
 		try {
 			$votingSession = VotingSession::requireCurrentVotingSession();
 			$currentUser = $votingSession->requireCurrentUser();
 			$group = $votingSession->getCurrentGroup();
-
+			
 			if (!($group instanceof GrupoDemanda)) {
 				throw new ErrorException("Você já votou em todos grupos disponíveis.");
 			}
@@ -35,37 +34,37 @@ class Election extends AppController {
 				$type = AppException::INFO;
 			throw new AppException($e->getMessage(), $type, array('controller' => 'Auth', 'action' => 'login'), array('cidadao' => $currentUser));
 		}
-
+		
 		$votoLog = $votingSession->getVotoLog();
 		if (!($votoLog instanceof VotoLog)) {
-			$id_meio_votacao = Config::get('isMobile') === TRUE ? MeioVotacao::MOBILE : MeioVotacao::WEB;
+			$id_meio_votacao = Config::get('isMobile')===TRUE?MeioVotacao::MOBILE:MeioVotacao::WEB;
 			$votoLog = new VotoLog($currentUser->getIdCidadao(), $group->getIdVotacao(), $group->getIdGrupoDemanda(), $id_meio_votacao, $_SERVER['REMOTE_ADDR']);
 			$votoLog->setIdVotoLog($votoLog->insert());
 			$votingSession->setVotoLog($votoLog);
 		}
-
+		
 		self::redirect(array('controller' => 'Election', 'action' => 'step', 'step' => 1));
 	}
-
+	
 	public static function step() {
 		$votingSession = VotingSession::requireCurrentVotingSession();
 		$currentUser = $votingSession->requireCurrentUser();
 		$regiao = $currentUser->getRegiao();
 		$group = $votingSession->getCurrentGroup();
 		$areasTematicas = $group->getAreasTematicas($regiao->getIdRegiao());
-
+		
 		self::registerVotes();
-
+		
 		$step = self::getParam('step');
 		$page = $votingSession->getStep($step);
-		$nextStep = $page['pages'] == $step ? NULL : $step + 1;
+		$nextStep = $page['pages']==$step?NULL:$step+1;
 		$cedulas = $page['content'];
 		$totalSteps = $page['pages'];
-
+		
 		$qtdMaxEscolha = $group->getQtdMaxEscolha();
-
+		
 		$html = new HTMLHelper();
-
+		
 		if (!is_null($nextStep))
 			$nextURL = $html->url(array('controller' => 'Election', 'action' => 'step', 'step' => $nextStep));
 		else {
@@ -74,134 +73,117 @@ class Election extends AppController {
 			else
 				$nextURL = $html->url(array('controller' => 'Election', 'action' => 'confirm'));
 		}
-
-		self::setJavascriptVar('previousStep', $step - 1);
+		
+		self::setJavascriptVar('previousStep', $step-1);
 		self::setJavascriptVar('nextStep', $nextStep);
 		self::setJavascriptVar('previousStepURL', $html->url(array('controller' => 'Election', 'action' => 'step', 'step' => $step - 1)));
 		self::setJavascriptVar('reviewURL', $html->url(array('controller' => 'Election', 'action' => 'review')));
 		self::setJavascriptVar('qtdMaxEscolha', $qtdMaxEscolha);
-
+		
 		$areas = $group->getOptionsGroupByAreaTematica($regiao->getIdRegiao(), $cedulas);
-
+		
 		$groups[$group->getIdGrupoDemanda()] = array(
-			'group' => $group,
-			'areasTematicas' => $areasTematicas
-		);
-
+					'group' => $group,
+					'areasTematicas' => $areasTematicas
+				);
+		
 		if ($group->getFgTituloSimples() != 1)
 			$groups[$group->getIdGrupoDemanda()]['areas'] = $areas;
 		else
 			$groups[$group->getIdGrupoDemanda()]['options'] = $cedulas;
-
+		
 		self::render(compact('step', 'nextURL', 'totalSteps', 'groups', 'qtdMaxEscolha', 'nextStep'));
 	}
-
+	
 	public static function review() {
 		$votingSession = VotingSession::requireCurrentVotingSession();
 		$currentUser = $votingSession->requireCurrentUser();
 		$regiao = $currentUser->getRegiao();
 		$group = $votingSession->getCurrentGroup();
 		$options = $votingSession->getOptions($group->getIdGrupoDemanda());
-
+		
 		self::registerVotes();
 		$votes = Vote::getSessionVotes();
-
+		
 		$next_group = reset($group->findNextGroup());
 		if ($next_group instanceof GrupoDemanda)
 			$next_group = TRUE;
 		else
 			$next_group = FALSE;
-
+		
 		$selection = array();
 		foreach ($options as $option) {
 			$selection[$option->getIdCedula()] = $option;
 		}
-
+		
 		$areasTematicas = $group->getAreasTematicas($regiao->getIdRegiao());
 		$selectedOptions = array();
 		foreach ($votes as $vote) {
 			$option = $selection[$vote->getIdCedula()];
 			$selectedOptions[] = $option;
 		}
-
+		
 		$html = new HTMLHelper();
 		$lastStep = $votingSession->getLastStep();
 		self::setJavascriptVar('previousStep', $lastStep);
 		self::setJavascriptVar('previousStepURL', $html->url(array('controller' => 'Election', 'action' => 'step', 'step' => $lastStep)));
-
+		
 		self::render(compact('options', 'votes', 'group', 'selection', 'next_group', 'areasTematicas', 'selectedOptions'));
 	}
-
+	
 	public static function confirm() {
-		require_once LIB_PATH.'recaptchalib.php';
-		
 		$votingSession = VotingSession::requireCurrentVotingSession();
 		$currentUser = $votingSession->requireCurrentUser();
 		$group = $votingSession->getCurrentGroup();
-
-		$privatekey = "6LfQlNMSAAAAACiff1nkHD-R-172AAz6Jhdz3CQE";
-		$captcha = self::getParam('recaptcha_response_field');
-		if (isset($captcha))
-			$votingSession->setCaptcha($captcha);
-
+	
 		self::registerVotes();
 		$votes = Vote::getSessionVotes();
 		$votoLog = VotoLog::cast($votingSession->getVotoLog());
-
+		
 		if (count($votes) > $group->getQtdMaxEscolha()) {
-			$e = new AppException("Excedido o número de " . $group->getQtdMaxEscolha() . " opções.", AppException::ERROR, array('controller' => 'Election', 'action' => 'review'));
+			$e = new AppException("Excedido o número de ".$group->getQtdMaxEscolha()." opções.", AppException::ERROR, array('controller' => 'Election', 'action' => 'review'));
 			self::flash($e);
 			throw $e;
 		}
-
+		
 		foreach ($votes as $vote) {
 			$voto = new Voto($vote->getIdCedula(), $currentUser->getRegiao()->getIdMunicipio(), $votoLog->getIdMeioVotacao(), $_SERVER['REMOTE_ADDR']);
 			$voto->setIdVoto($voto->insert());
 		}
-
+		
 		$votoLog->setDthFim(new DateTime());
 		$votoLog->setQtdSelecoes(count($votes));
 		$votoLog->update();
 		$next_group = $group->findNextGroup(TRUE);
 		$votingSession->finishGroup();
-
-		if (is_array($next_group) && reset($next_group) instanceof GrupoDemanda) {
+		
+		if (is_array($next_group) && reset($next_group) instanceof GrupoDemanda)
 			self::redirect(array('controller' => 'Election', 'action' => 'start'));
-		} else {
-			$resp = recaptcha_check_answer($privatekey, $_SERVER["REMOTE_ADDR"], $_POST["recaptcha_challenge_field"], $captcha);
-
-			
-			//descomentar o $resp quando colocar em prod
-			if ($resp->is_valid || isset($resp)) {
-				$votingSession->finish();
-				self::redirect(array('controller' => 'Election', 'action' => 'success'));
-			}else
-				self::redirect(array('controller' => 'Election', 'action' => 'start'));
+		else {
+			$votingSession->finish();
+			self::redirect(array('controller' => 'Election', 'action' => 'success'));
 		}
 	}
-
+	
 	public static function success() {
 		self::render();
 	}
-
+	
 	private static function registerVotes() {
-		if (!self::isPost())
-			return;
+		if (!self::isPost()) return;
 		$votingSession = VotingSession::requireCurrentVotingSession();
 		$group = $votingSession->getCurrentGroup();
-
+	
 		$previousStep = self::getParam('votes_step');
-		if (is_null($previousStep))
-			return;
-
+		if (is_null($previousStep)) return;
+	
 		$selected = self::getParam('selected');
-		if (is_null($selected))
-			$selected = array();
-
+		if (is_null($selected)) $selected = array();
+	
 		if (is_numeric($previousStep)) {
 			$options = $votingSession->getStep($previousStep);
 			$options = $options['content'];
-
+				
 			foreach ($options as $cedula) {
 				if (array_search($cedula->getIdCedula(), $selected) !== FALSE)
 					Vote::addVote($cedula->getIdCedula());
@@ -210,7 +192,7 @@ class Election extends AppController {
 			}
 		} else {
 			$options = Vote::getSessionVotes();
-
+				
 			foreach ($options as $cedula) {
 				if (array_search($cedula->getIdCedula(), $selected) !== FALSE)
 					Vote::addVote($cedula->getIdCedula());
@@ -219,5 +201,4 @@ class Election extends AppController {
 			}
 		}
 	}
-
 }
