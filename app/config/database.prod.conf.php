5aefca746bb1725993972afe62e6746f09e08fbd
procergs-rosane-leite <procergs-rosane-leite@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2012-07-24 13:59:51 +0000

    git-svn-id: http://svn.procergs.reders/vpr/trunk@237 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- app/config/database.prod.conf.php
+++ app/config/database.prod.conf.php
@@ -1,6 +1,6 @@
 <?php
 $databases['seplag_vpr'] = array(
-	'dsn' => 'mysql:host=seplag_vpr.mysqlinfra.procergs.reders;dbname=seplag_vpr',
+	'dsn' => 'mysql:host=seplag_vpr.mysql.procergs.reders;dbname=seplag_vpr',
 	'user' => 'p_seplag_vpr_si',
 	'password' => 's3pl4gs1',
 	'charset' => 'utf8'
