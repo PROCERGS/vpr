5e7ae3b6fbb053f7b4addeed8f3eb6ccb5e2de90
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2013-07-24 15:31:15 +0000

    Permitido autenticar-se com RG ou CPF. refs #23168
    git-svn-id: http://svn.procergs.reders/vpr/trunk@243 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-	$("fieldset.optional input").keyup(function(){
-		var expand = false;
-		$("fieldset.optional input").each(function(){
-			if ($(this).val().length > 0)
-				expand = true;
-		});
-		if (expand)
-			$("fieldset.optional legend").slideDown();
-		else
-			$("fieldset.optional legend").slideUp();
-		return false;
-	});
+    $("fieldset.optional input").keyup(function(){
+        var expand = false;
+        $("fieldset.optional input").each(function(){
+            if ($(this).val().length > 0)
+                expand = true;
+        });
+        if (expand)
+            $("fieldset.optional legend").slideDown();
+        else
+            $("fieldset.optional legend").slideUp();
+        return false;
+    });
 	
-	$("fieldset.optional legend").hide();
+    $("fieldset.optional legend").hide();
 });
