93f27845dbebd4ef06ae92d6efbfdfa9aa8bec5e
procergs-ricardo-jesus <procergs-ricardo-jesus@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2014-04-02 14:33:15 +0000

    init Locais de Votação
    git-svn-id: http://svn.procergs.reders/vpr/branches/2.0@388 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- src/PROCERGS/VPR/CoreBundle/Controller/DefaultController.php
+++ src/PROCERGS/VPR/CoreBundle/Controller/DefaultController.php
@@ -6,12 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormError;
 
 class DefaultController extends Controller
 {
 
     /**
-     * @Template()
+     * @Template()https://drive.google.com/keep/
      */
     public function indexAction()
     {
@@ -26,6 +27,46 @@ class DefaultController extends Controller
     {
         $voter = $this->get('security.context')->getToken()->getUser();
         return array('voter' => $voter);
+    }
+
+    /**
+     * @Template()
+     */
+    public function placesAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+                     ->add('city', 'text', array(
+                        'required' => true
+                      ))
+                     ->add('submit', 'submit')
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        $boxes = array();
+        if ($form->isValid()) {
+          $em = $this->getDoctrine()->getManager();
+          $data = $form->getData();
+
+          $cityRepo = $em->getRepository('PROCERGSVPRCoreBundle:City');
+          $city = $cityRepo->findOneBy(array('name' => $data['city']));
+          if ($city) {
+            $repository = $this->getDoctrine()->getRepository('PROCERGSVPRCoreBundle:BallotBox');
+            $query = $repository->createQueryBuilder('b')
+                ->where('b.city = :city')
+                ->setParameter('city', $city->getId())
+                ->getQuery();
+            $boxes = $query->getResult();
+          } else {
+            $form->addError(new FormError('not found city'));
+          }
+
+        }
+
+        $form = $form->createView();
+
+
+        return $this->render('PROCERGSVPRCoreBundle:Default:places.html.twig', compact('form', 'boxes'));
     }
 
 }
