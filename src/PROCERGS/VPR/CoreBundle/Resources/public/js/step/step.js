f5785e513c71358cbaba4a827489eaee1ec80ba5
procergs-ricardo-jesus <procergs-ricardo-jesus@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2014-04-15 18:16:42 +0000

    git-svn-id: http://svn.procergs.reders/vpr/branches/2.0@465 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- src/PROCERGS/VPR/CoreBundle/Resources/public/js/step/step.js
+++ src/PROCERGS/VPR/CoreBundle/Resources/public/js/step/step.js
@@ -1,10 +1,10 @@
 $(function() {
 
   $(".ballot input").on("click", function(event) {
-    var sel = $('.ballot input:checked');
+    var sel = $(".ballot input:checked");
     if (sel.length > oba.maxItens) {
       event.preventDefault();
-      alert(oba.label['limit'] + oba.maxItens);
+      alert(oba.label["limit"] + oba.maxItens);
       resetCheckbox($(this).get(0));
     }
   });
@@ -13,14 +13,14 @@ $(function() {
     $(".ballot").addClass("animate");
 
     $(".loader").fadeIn("normal", function() {
-      $('#btn-vote').hide();
+      $("#btn-vote").hide();
       $(".ballot input").not(":checked").parent().addClass("hidden");
       $("input:checked").closest(".options").prev(".step-category").addClass("checked");
       $(".step-category").not(".checked").addClass("hidden");
 
       $(".loader").delay(1000).fadeOut("normal", function() {
         $(".ballot").removeClass("animate");
-        $('#confirmation').show();
+        $("#confirmation").show();
       });
     });
   });
@@ -29,7 +29,7 @@ $(function() {
     $(".ballot").removeClass("animate");
     $(".ballot li, .step-category").removeClass("hidden")
     $("#confirmation").hide();
-    $('#btn-vote').show();
+    $("#btn-vote").show();
   });
 
 });
