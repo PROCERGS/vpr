14af8af4c5a5074db2332361a352c58d7bd354fb
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2014-05-13 22:24:17 +0000

    Code formating
    git-svn-id: http://svn.procergs.reders/vpr/branches/2.0@489 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- src/PROCERGS/VPR/CoreBundle/Entity/BallotBoxRepository.php
+++ src/PROCERGS/VPR/CoreBundle/Entity/BallotBoxRepository.php
@@ -10,10 +10,11 @@ class BallotBoxRepository extends EntityRepository
     public function findOnlineByPoll($poll)
     {
         $isOnline = true;
+        $sql = 'SELECT b FROM PROCERGSVPRCoreBundle:BallotBox b WHERE b.poll = :poll AND b.isOnline = :isOnline';
         return $this->getEntityManager()
-                        ->createQuery(
-                                'SELECT b FROM PROCERGSVPRCoreBundle:BallotBox b WHERE b.poll = :poll AND b.isOnline = :isOnline'
-                        )->setParameters(compact('poll', 'isOnline'))->getOneOrNullResult();
+                        ->createQuery($sql)
+                        ->setParameters(compact('poll', 'isOnline'))
+                        ->getOneOrNullResult();
     }
 
 }
