a06f55b4a6191796bdce61354afca0995e9c087d
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2014-05-18 21:01:47 +0000

    Removed nonsense behavior
    git-svn-id: http://svn.procergs.reders/vpr/branches/2.0@515 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- src/PROCERGS/VPR/CoreBundle/Resources/public/js/register/register.js
+++ src/PROCERGS/VPR/CoreBundle/Resources/public/js/register/register.js
@@ -1,73 +1,56 @@
 /*global $:false */
 $(function() {
-  "use strict";
-  /*
-   * Register check username
-   */
-  $(usernameId).on('blur', function () {
-    if (!this.value.length) {
-      return;
-    }
-    this.value = completaZerosEsquerda(this.value, 12);
-    if (validarTitulo(this.value)) {
+    "use strict";
+    /*
+     * Mobile behavior to register form
+     */
+    var formContainer = $(".register-form-container");
 
-    } else {
-      alert(messages[0]);
-    }
+    $(".register-label").on("click", function() {
+        if ($(window).width() <= 768) {
+            formContainer.toggleClass("animate");
+            $(".register-tooltip").tooltip("hide");
+        }
+    });
 
-  });
+    $(window).resize(function() {
+        if ($(window).width() > 768 && formContainer.is(":hidden")) {
+            formContainer.show();
+        }
 
+        $(".register-tooltip").tooltip("hide");
 
-  /*
-   * Mobile behavior to register form
-   */
-  var formContainer = $(".register-form-container");
-
-  $(".register-label").on("click", function() {
-    if ($(window).width() <= 768) {
-      formContainer.toggleClass("animate");
-      $(".register-tooltip").tooltip("hide");
-    }
-  });
-
-  $(window).resize(function() {
-    if ($(window).width() > 768 && formContainer.is(":hidden") ) {
-      formContainer.show();
-    }
-
-    $(".register-tooltip").tooltip("hide");
-
-  });
+    });
 
 
-  /*
-   * Show alert about LC
-   */
-   $("#about-lc-link a").on("click", function(e) {
-    e.preventDefault();
-    $("#about-lc").toggleClass("hidden");
-   });
+    /*
+     * Show alert about LC
+     */
+    $("#about-lc-link a").on("click", function(e) {
+        e.preventDefault();
+        $("#about-lc").toggleClass("hidden");
+    });
 
 
-  /*
-   * Add tooltip at register form
-   */
-   $(".register-tooltip").on({
-    focusin: function() {
-      $(".tre-search-link").fadeIn().css("display", "block");
-    },
-    focusout: function() {
-      $(".tre-search-link").fadeOut();
-    }
-   });
+    /*
+     * Add tooltip at register form
+     */
+    $(".register-tooltip").on({
+        focusin: function() {
+            $(".tre-search-link").fadeIn().css("display", "block");
+        },
+        focusout: function() {
+            $(".tre-search-link").fadeOut();
+        }
+    });
 
 
-   // $(".register-tooltip").tooltip({
-   //  "container" : ".register-label",
-   //  "html" : true,
-   //  "placement" : "top",
-   //  "title" : "<span>" + tooltipContent.text + "</span><a href='" + tooltipContent.link + "' target='blank'>" + tooltipContent.link_text + "</a>",
-   //  "trigger" : "focus"
-   // });
+    // $(".register-tooltip").tooltip({
+    //  "container" : ".register-label",
+    //  "html" : true,
+    //  "placement" : "top",
+    //  "title" : "<span>" + tooltipContent.text + "</span><a href='" + tooltipContent.link + "' target='blank'>" + tooltipContent.link_text + "</a>",
+    //  "trigger" : "focus"
+    // });
 
 });
