ce8c7ba9286567f881c5ba2557baef81fc74057f
procergs-cristiano-davila <procergs-cristiano-davila@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2014-05-19 12:13:38 +0000

    git-svn-id: http://svn.procergs.reders/vpr/branches/2.0@523 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- lvp_mock.bat
+++ lvp_mock.bat
@@ -1,5 +1,6 @@
 @echo off
 rmdir "app/cache/prod" /s /q
 rmdir "app/cache/dev" /s /q
-svn update
+svn revert .
+svn update .
 composer install --prefer-dist --optimize-autoloader && php app/console assetic:dump --env=prod --no-debug && php app/console cache:clear --env=prod --no-warmup
