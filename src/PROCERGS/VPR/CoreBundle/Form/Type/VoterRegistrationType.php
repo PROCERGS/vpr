673478eb1db5becbda21d2d7a91a9e13f5db1ad9
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2014-05-19 22:19:55 +0000

    Voter Registration from field type working now
    git-svn-id: http://svn.procergs.reders/vpr/branches/2.0@534 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- src/PROCERGS/VPR/CoreBundle/Form/Type/VoterRegistrationType.php
+++ src/PROCERGS/VPR/CoreBundle/Form/Type/VoterRegistrationType.php
@@ -3,19 +3,38 @@
 namespace PROCERGS\VPR\CoreBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class CityType extends AbstractType
+class VoterRegistrationType extends NumberType
 {
+
+    public function getParent()
+    {
+        return 'form';
+    }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name');
+        return parent::buildForm($builder, $options);
     }
 
     public function getName()
     {
-        return 'city';
+        return 'voter_registration';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+        $resolver->setDefaults(array(
+            'max_length' => 12,
+            'attr' => array(
+                'type' => 'number',
+                'pattern' => '[0-9]*'
+            )
+        ));
     }
 
 }
