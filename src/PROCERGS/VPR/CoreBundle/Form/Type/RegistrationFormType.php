0c6b1b375342abbc665186b3b292baa59316111e
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2014-05-19 20:45:02 +0000

    Fixed registration inputs behavior on mobile
    git-svn-id: http://svn.procergs.reders/vpr/branches/2.0@531 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- src/PROCERGS/VPR/CoreBundle/Form/Type/RegistrationFormType.php
+++ src/PROCERGS/VPR/CoreBundle/Form/Type/RegistrationFormType.php
@@ -10,12 +10,25 @@ class RegistrationFormType extends BaseType
 {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
-    {        
-        $builder->add('username', null, array('required' => true, 'max_length' => 12));
+    {
+        $builder->add('username', 'number', array(
+            'required' => true,
+            'max_length' => 12,
+            'attr' => array(
+                'type' => 'number',
+                'pattern' => '[0-9]*'
+            )
+        ));
         $builder->add('firstname', 'text', array('required' => true));
         $builder->add('email', 'email', array('required' => false));
-        $builder->add('mobile', null, array('required' => false));
-    }    
+        $builder->add('mobile', 'number', array(
+            'required' => false,
+            'attr' => array(
+                'type' => 'tel',
+                'pattern' => '[0-9- ()+]*'
+            )
+        ));
+    }
 
     public function getName()
     {
--- src/PROCERGS/VPR/CoreBundle/Resources/public/js/register/register.js
+++ src/PROCERGS/VPR/CoreBundle/Resources/public/js/register/register.js
@@ -44,4 +44,12 @@ $(function() {
         }
     });
 
+    $('form').on('focus', 'input[type=number]', function(e) {
+        $(this).on('mousewheel.disableScroll', function(e) {
+            e.preventDefault()
+        })
+    })
+    $('form').on('blur', 'input[type=number]', function(e) {
+        $(this).off('mousewheel.disableScroll')
+    })
 });
