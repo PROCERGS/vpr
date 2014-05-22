3d28e6a4ed7650ea721f255df85b8fa92de82721
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2014-05-22 23:53:17 +0000

    Fixing translations
    git-svn-id: http://svn.procergs.reders/vpr/branches/2.0@565 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- src/PROCERGS/VPR/CoreBundle/Form/Type/CityType.php
+++ src/PROCERGS/VPR/CoreBundle/Form/Type/CityType.php
@@ -2,15 +2,20 @@
 
 namespace PROCERGS\VPR\CoreBundle\Form\Type;
 
-use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class CityType extends AbstractType
+class CityType extends TextType
 {
+
+    public function getParent()
+    {
+        return 'form';
+    }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name');
+        return parent::buildForm($builder, $options);
     }
 
     public function getName()
