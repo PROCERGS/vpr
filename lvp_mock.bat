7b1366dc94e0887290f455d142c41ede40c93c95
procergs-cristiano-davila <procergs-cristiano-davila@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2014-05-26 11:39:10 +0000

    adjusts
    git-svn-id: http://svn.procergs.reders/vpr/branches/2.0@587 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- lvp_mock.bat
+++ lvp_mock.bat
@@ -1,5 +1,6 @@
 @echo off
+svn update
+composer install --prefer-dist --optimize-autoloader 
 rmdir "app/cache/prod" /s /q
 rmdir "app/cache/dev" /s /q
-svn update
-composer install --prefer-dist --optimize-autoloader && php app/console assetic:dump --env=prod --no-debug && php app/console cache:clear --env=prod --no-warmup
+php app/console assets:install --env=prod && php app/console assetic:dump --env=prod --no-debug && php app/console cache:clear --env=prod --no-warmup
