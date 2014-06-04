11dba5bd3f64787fcee7b8e8d73ee4801f60449a
procergs-cristiano-davila <procergs-cristiano-davila@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2014-06-04 12:37:04 +0000

    some trans
    git-svn-id: http://svn.procergs.reders/vpr/branches/2.0@715 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- src/PROCERGS/VPR/CoreBundle/Controller/StatsController.php
+++ src/PROCERGS/VPR/CoreBundle/Controller/StatsController.php
@@ -20,36 +20,6 @@ class StatsController extends Controller
 {
 
     /**
-     * @Route("/stats/option_vote_by_corede", name="vpr_option_vote_by_corede")
-     * @Template()
-     */
-    public function optionVoteByCoredeAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $statsRepos = $em->getRepository('PROCERGSVPRCoreBundle:StatsOptionVote');
-
-        $form = $this->createForm(new PollOptionFilterType());
-
-        $results = array();
-        if ($request->isMethod('POST')) {
-            $form->bind($request);
-
-            $poll = $form->get('poll')->getData();
-            $corede = $form->get('corede')->getData();
-
-            $steps = $poll->getSteps();
-            foreach($steps as $step){
-                $results[$step->getName()] = $statsRepos->findOptionVoteByCorede($corede->getId(),$step->getId());
-            }
-        }
-
-        return array(
-            'form' => $form->createView(),
-            'results' => $results
-        );
-    }
-    
-    /**
      * @Route("/stats/option/votes", name="vpr_option_vote_by_corede")
      * @Template()
      */
@@ -257,7 +227,17 @@ class StatsController extends Controller
      */
     public function graphicsAction()
     {
-        return array();
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->createQueryBuilder()
+        ->select('v')
+        ->from('PROCERGSVPRCoreBundle:StatsTotalCoredeVote', 'v')
+        ->orderBy('v.totalVotes','DESC')
+        ->setMaxResults(1)->getQuery()->getOneOrNullResult();
+        $response = array();
+        if ($entity) {
+            $response['created_at'] = $entity->getCreatedAt();
+        }
+        return $response;
     }
     
     /**
