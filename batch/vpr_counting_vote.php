15ef7f84c00a8313f3e23daf9e13f0871a020884
procergs-guilherme-donato <procergs-guilherme-donato@d2da55db-5866-43f3-bc33-3da1c376b5d1>
2014-06-05 19:21:20 +0000

    Added INSERT into DB
    git-svn-id: http://svn.procergs.reders/vpr/branches/2.0@733 d2da55db-5866-43f3-bc33-3da1c376b5d1

--- batch/vpr_counting_vote.php
+++ batch/vpr_counting_vote.php
@@ -1,50 +1,70 @@
 <?php
-if (! isset($argv[1], $argv[2], $argv[3])) {
-    echo "Missing config file or pem stuff";
+
+require_once 'stuff.php';
+
+ini_set('max_execution_time', -1);
+
+$config = checkArguments($argv);
+
+$privateKeyFile = $config['privateKeyFile'];
+$passphrase = $config['passphrase'];
+
+$file = 'file://' . $privateKeyFile;
+$pollPrivateKey = openssl_pkey_get_private($file, $passphrase);
+if (!$pollPrivateKey) {
+    e("Couldn't open private key. Is the passphrase correct?");
+    e($passphrase);
+    e("Error returned by OpenSSL: " . openssl_error_string());
     exit(1);
-}
-$config = parse_ini_file($argv[1]);
-if (! $config) {
-    echo "Can't parse config file";
-    exit(1);
-}
-if (! file_exists($argv[2])) {
-    echo "Don't exist pem file";
-    exit(1);
+} else {
+    $config['pollPrivateKey'] = $pollPrivateKey;
 }
 
-$file = 'file://' . __DIR__ . DIRECTORY_SEPARATOR . $argv[2];
-$privatePollKey = openssl_pkey_get_private($file, $config['password']);
-if (! $privatePollKey) {
-    echo "Mismatch pem file and pem password";
-    exit(1);
-}
+$counting = array();
+
 try {
-    $conn = new PDO($config['database_driver'] . ':host=' . $config['database_host'] . ';dbname=' . $config['database_name'] . ';charset=utf8', $config['database_user'], $config['database_password'], array(
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
-    ));
-    $sql = "select * from vote limit 10";
+    $conn = getPDOConnection($config);
+    $sql = getObviouslyValidVotesQuery();
     $stmt = $conn->prepare($sql);
+
+    $insertOpenVote = $conn->prepare(getInsertOpenVoteQuery());
+
+    e("Running query...");
     $stmt->execute();
+    e("Done!");
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $openVote = null;
+        $openOptions = null;
         $options = base64_decode($row['options']);
-        $passphrase = base64_decode($row['passphrase']);
-        if (openssl_open($options, $openVote, $passphrase, $privatePollKey) === false) {
+        $votePassphrase = base64_decode($row['passphrase']);
+        if (openssl_open($options, $openOptions, $votePassphrase, $pollPrivateKey) === false) {
             throw new Exception(openssl_error_string());
         }
-        if (! $openVote) {
-            throw new Exception('weird! no vote for id' . $row['id']);
+        if (!$openOptions) {
+            throw new Exception("weird! no vote for id" . $row['id']);
         }
-        $openVote = json_decode($openVote, true);
-        if ($openVote === false) {
-            throw new Exception('weird! no valid serializaded vote for id' . $row['id']);
+        $openOptions = json_decode($openOptions, true);
+        if ($openOptions === false) {
+            throw new Exception("weird! no valid serializaded vote for id" . $row['id']);
         }
-        print_r($openVote);
-        die();
+
+        $conn->beginTransaction();
+        foreach ($openOptions as $option) {
+            $openVote = array();
+            $openVote['ballot_box_id'] = $row['ballot_box_id'];
+            $openVote['corede_id'] = $option['corede']['id'];
+            $openVote['poll_option_id'] = $option['id'];
+            $openVote['auth_type'] = $row['auth_type'];
+            $openVote['voter_registration'] = !empty($row['voter_registration']);
+            $openVote['signature'] = null;
+            $openVote['signature'] = signVote($openVote, $config);
+
+            @$counting[$openVote['poll_option_id']] += 1;
+            $insertOpenVote->execute($openVote);
+        }
+        $conn->commit();
     }
-    $stmt = null;
 } catch (Exception $e) {
-    echo $e->getMessage();
+    $conn->rollBack();
+    e($e->getMessage());
     exit(1);
 }
