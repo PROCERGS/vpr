f657b3c09dc11f6d8623c1fe79533d88c4fbc775
Ricardo Pareja <pareja.r@gmail.com>
2016-06-24 17:06:00 -0300

    add pagina indisponivel

---
+++ web/indisponivel.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Consulta Popular</title>
+    <style type="text/css">
+        * {
+            font-family: 'Roboto', sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-image: url(bundles/procergsvprcore/images/header.jpg);
+            background-size: cover;
+            background-repeat: no-repeat;
+            height: 140px;
+            vertical-align: middle;
+            text-align: center;
+            line-height: 1em;
+            vertical-align: middle;
+        }
+
+        header a {
+            font-size: 2.2em;
+            line-height: 120%;
+            color: #4D8E28;
+            text-decoration: none;
+            font-weight: 700;
+            text-shadow: 1px 1px 2px #666;
+        }
+
+        #msg {
+            background-color: rgba(222, 233, 205, 0.52);
+            margin: 50px auto;
+            min-height: 500px;
+            max-width: 940px;
+            font-size: 50px;
+            padding: 50px;
+            text-align: center;
+            color:#424242;
+            line-height: 500px;
+            vertical-align: middle;
+        }
+    </style>
+</head>
+<body>
+
+    <header class="text-center">
+        <a href="#" title="Consulta Popular Orçamento 2017">Consulta Popular<br> Orçamento 2017</a>
+    </header>
+
+    <div id="msg">Sistema indisponível</div>
+</body>
+</html>
