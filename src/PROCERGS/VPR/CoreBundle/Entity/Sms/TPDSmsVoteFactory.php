f99e9e1b2dbb50bfe68458a416cad3b4388b580f
root <root@localhost.localdomain>
2017-07-20 07:33:47 -0300

    data vindo errado

--- src/PROCERGS/VPR/CoreBundle/Entity/Sms/TPDSmsVoteFactory.php
+++ src/PROCERGS/VPR/CoreBundle/Entity/Sms/TPDSmsVoteFactory.php
@@ -10,7 +10,13 @@ class TPDSmsVoteFactory
      */
     public static function createSmsVote($sms)
     {
-        $date = \DateTime::createFromFormat('Y-m-d\TH:i:s.000-03:00', $sms->dataHoraRecebimento);
+        if (!$sms->dataHoraRecebimento) {
+            throw new \Exception("Nao foi enviado data hora recebimento ");
+        }
+        $date = \DateTime::createFromFormat('Y-m-d\TH:i:s', substr($sms->dataHoraRecebimento, 0, 19) );
+        if (!$date) {
+            throw new \Exception("Formato de hora invalida: " . $sms->dataHoraRecebimento);
+        }
         $to = BrazilianPhoneNumberFactory::createFromE164("+{$sms->de}");
 
         $smsVote = new SmsVote();
